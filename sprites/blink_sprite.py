1708f41e55e036ba934198c4f9c00f874ec17120
Ranc58 <rancvova@gmail.com>
2019-04-08 21:23:48 +0300

    Simplify stars blink function

--- sprites/blink_sprite.py
+++ sprites/blink_sprite.py
@@ -1,7 +1,4 @@
 import curses
-import asyncio
-import time
-
 import utils
 
 
@@ -11,16 +8,6 @@ async def blink(canvas, row, column, symbol='*', waiting_time=10):
     await utils.wait_time(waiting_time)
 
     while True:
-        for star in stars:
-            text_type = star[0]
-            time_for_sleep = star[1]
-            iterations_count = 0
-            needles_count = utils.convert_ms_to_iterations(time_for_sleep * 1000)
-            for _ in reversed(range(0, int(needles_count) + 1)):
-
-                if iterations_count == 0:
-                    time.sleep(0.001)
-                    canvas.addstr(row, column, symbol, text_type)
-                    await asyncio.sleep(0)
-                iterations_count -= 1
-            await utils.wait_time(time_for_sleep)
+        for text_attrs, time_for_sleep in stars:
+            canvas.addstr(row, column, symbol, text_attrs)
+            await utils.wait_time(time_for_sleep * 10)
