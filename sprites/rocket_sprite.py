2d0bca45f4552b420308579e0013e10cec24a3a5
Ranc58 <rancvova@gmail.com>
2019-04-08 22:13:36 +0300

    simplify rocket position corrections

--- sprites/rocket_sprite.py
+++ sprites/rocket_sprite.py
@@ -3,33 +3,36 @@ import utils
 from utils import draw_frame, read_controls, get_frame_size
 
 
-def correct_spaceship_position(
-        row_max, row, row_frame, column_max, column, column_frame
-):
-    corrected_row, corrected_column = row, column
-    if row_max - row <= row_frame:
-        corrected_row = row_max - row_frame - 1
-    elif row <= 0:
+def correct_row(max_available_row, current_row, row_frame):
+    corrected_row = current_row
+    if current_row >= max_available_row - row_frame:
+        corrected_row = max_available_row - row_frame - 1
+    elif current_row <= 0:
         corrected_row = 1
-    if column > column_max - (column_frame + 1):
-        corrected_column = column_max - (column_frame + 1)
-    elif column <= 1:
+    return corrected_row
+
+
+def correct_column(max_available_column, current_column, column_frame):
+    corrected_column = current_column
+    if current_column > max_available_column - column_frame:
+        corrected_column = max_available_column - (column_frame + 1)
+    elif current_column <= 1:
         corrected_column = 1
-    return corrected_row, corrected_column
+    return corrected_column
 
 
 async def get_spaceship(canvas, sprites, speed=2):
-    row_max, column_max = utils.get_terminal_size()
-    row, column = row_max - 10, column_max / 2
+    max_available_row, max_available_column = utils.get_terminal_size()
+    row, column = max_available_row - 10, max_available_column / 2
+
     while True:
         row_frame, column_frame = get_frame_size(sprites[0])
         canvas.nodelay(True)
         row_pos, column_pos, space = read_controls(canvas)
         row += row_pos * speed
         column += column_pos * speed
-        row, column = correct_spaceship_position(
-            row_max, row, row_frame, column_max, column, column_frame
-        )
+        row = correct_row(max_available_row, row, row_frame)
+        column = correct_column(max_available_column, column, column_frame)
         for sprite in sprites:
             draw_frame(canvas, row, column, sprite, negative=False)
             await asyncio.sleep(0)
